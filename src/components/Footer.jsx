c17056b335b4347051185498d59f839dd05d0ebf
Noel Fran Thomas <noelfranthomas@gmail.com>
2024-08-20 23:51:49 -0600

    removed placeholders from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,7 @@ const navigation = [
   {
     title: 'Work',
     links: [
-      { title: 'FamilyFund', href: '/work/family-fund' },
-      { title: 'Unseal', href: '/work/unseal' },
-      { title: 'Phobia', href: '/work/phobia' },
+      { title: 'Hotchkiss Brain Institute', href: '/work/hotchkiss-brain-institute' },
       {
         title: (
           <>
